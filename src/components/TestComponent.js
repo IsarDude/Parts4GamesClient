b52a0ce3d9c459a60da58ae1cf4fd70b0eb9c58e
Lukas Fritsch <Lukas.fritsch@hof-university.de>
2021-01-14 17:02:22 +0100

    fristExampleClass

---
+++ src/components/TestComponent.js
@@ -0,0 +1,19 @@
+import {Button} from 'react-bootstrap';
+import axios from 'axios';
+
+axios.get("")
+
+function Test(){
+    return(
+        <div>
+        <div>
+            <h5>Lukas was here</h5>
+        </div>
+         <div>
+        
+        </div>
+        </div>
+    )
+}
+
+export default Test;
